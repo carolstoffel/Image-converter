ab5b95d91106efdb5736bd8d33611f6f5d402979
carolstoffel <carolinegiacomini@gmail.com>
2020-06-28 17:46:17 -0300

    Criada a interface gráfica com Tkinter

--- convert_image.py
+++ convert_image.py
@@ -4,41 +4,68 @@ from os.path import splitext
 import os
 import shutil
 
+def main(path, backup, formato):
+    global target_directory
+    global back
+    global new_extension
+    target_directory = path
+    back = backup
+    new_extension = formato
+    target_directory = tratar_path(target_directory)
+    change_dir_to = os.chdir("{}".format(target_directory))
+    files(target_directory)
 
-target_directory = str(
-    input('Which directory do you want to have the images converted?'))
-change_dir_to = os.chdir("{}".format(target_directory))
+def tratar_path(target_directory):
+    tg = target_directory.replace('/', '\\')
+    return tg
 
-old_extension = '.bmp'
-new_extension = '.jpg'
+def files(target_directory):
+    #old_extension = '.bmp'
+    #new_extension = '.jpg'
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if 'BKP_OLD_IMAGES' not in root:
+                dir = os.path.join(root, name)
+                dir_new = dir.split('\\')
+                filename = dir_new[-1]
+                file, extension = splitext(filename)
+                dir = target_directory+root[1:]
+                convert(file, extension, dir, new_extension, filename)
 
-for root, dirs, files in os.walk(".", topdown=False):
-    for name in files:
-        if 'BKP_OLD_IMAGES' not in root:
-            dir = os.path.join(root, name)
-            dir_new = dir.split('\\')
-            filename = dir_new[-1]
-            file, extension = splitext(filename)
-            dir = target_directory+root[1:]
-            if extension == old_extension:
-                try:
-                    print(dir+'\\'+file+extension)
-                    im = Image.open(dir+'\\'+file+extension)
-                    im.save(dir+'\\'+file+new_extension)
-                    print('converted image: {}'.format(filename))
-                except OSError as error:
-                    print("It's not possible to convert the file {}.\nError code: {}".format(
-                        filename, repr(error)))
-                try:
-                    if not os.path.isdir(dir + './' + 'BKP_OLD_IMAGES'):
-                        # creates the backup directory
-                        os.mkdir(dir+'./{}'.format('BKP_OLD_IMAGES'))
-                        print('Created the {} directory'.format('BKP_OLD_IMAGES'))
-                except:
-                    print("It wasn't possible to create the dir")
-                try:
-                    destino = dir + '\\BKP_OLD_IMAGES\\'+file+extension
-                    source = dir+'\\'+file+extension
-                    shutil.move(source, destino)
-                except:
-                    print("It wasn't possible to move")
+def convert(file, extension, dir, new_extension, filename):
+    if extension != new_extension:
+        try:
+            print(dir+'\\'+file+extension)
+            im = Image.open(dir+'\\'+file+extension)
+            im.save(dir+'\\'+file+new_extension)
+            print('converted image: {}'.format(filename))
+        except OSError as error:
+            print("It's not possible to convert the file {}.\nError code: {}".format(
+                filename, repr(error)))
+        if back == True:
+            do_backup(dir, file, extension)
+        elif back == False:
+            do_exclusion(dir, file, extension)
+
+
+def do_backup(dir, file, extension):
+    try:
+        if not os.path.isdir(dir + './' + 'BKP_OLD_IMAGES'):
+            # creates the backup directory
+            os.mkdir(dir+'./{}'.format('BKP_OLD_IMAGES'))
+            print('Created the {} directory'.format('BKP_OLD_IMAGES'))
+    except:
+        print("It wasn't possible to create the dir")
+    try:
+        destino = dir + '\\BKP_OLD_IMAGES\\'+file+extension
+        source = dir+'\\'+file+extension
+        shutil.move(source, destino)
+    except:
+        print("It wasn't possible to move")
+
+def do_exclusion(dir, file, extension):
+    source = dir+'\\'+file+extension
+    try:
+        os.remove(source)
+    except:
+        print("It wasn't possible to delete")
---
+++ interface.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import filedialog
+import convert_image
+
+root = Tk()
+root.title('Conversor de imagem')
+root.geometry('380x100')
+
+def browse_button():
+    global folder_path
+    global filename
+    filename = filedialog.askdirectory()
+    e.insert(0, filename)
+
+def ok_button(formato):
+    backup=var_backup.get()
+    convert_image.main(filename, backup, formato)
+
+
+
+frame = LabelFrame(root)
+frame.grid(row=10, column=3)
+
+MODES = [("BMP", ".bmp"), ("JPG", ".jpg"), ("PNG", ".png")]
+formato = StringVar()
+formato.set(".jpg")
+
+c=0
+for text, mode in MODES:
+    Radiobutton(root, text=text, variable=formato, value=mode).grid(row=0, column=c)
+    c += 1
+command=lambda: clicked(pizza.get())
+
+
+e = Entry(root, borderwidth=2,width=40, bg="white")
+e.grid(row=2, column=1)
+
+
+folder_path = StringVar()
+lbl1 = Label(master=root,textvariable=folder_path)
+lbl1.grid(row=2, column=0)
+button1 = Button(root,text="Browse", command=browse_button, anchor=W)
+button1.grid(row=2, column=2, padx=10)
+button2 = Button(root,text="OK", command= lambda: ok_button(formato.get()))
+button2.grid(row=3, column=2, pady=10)
+
+var_backup = BooleanVar()
+c1 = Checkbutton(root, text='Fazer backup', variable = var_backup, onvalue=True, offvalue=False, anchor=E)
+c1.deselect()
+c1.grid(row=3, column=0, columnspan=5)
+
+root.mainloop()
