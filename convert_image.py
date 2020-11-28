27dcb9ec1df00466bd9f8acabe42590987c7393b
carolstoffel <carolinegiacomini@gmail.com>
2020-11-28 13:24:52 -0300

    Changed to use the os.path.join function instead of joining the directory using //

--- convert_image.py
+++ convert_image.py
@@ -4,6 +4,7 @@ from os.path import splitext
 import os
 import shutil
 
+
 def main(path, backup, formato):
     global target_directory
     global back
@@ -15,13 +16,15 @@ def main(path, backup, formato):
     change_dir_to = os.chdir("{}".format(target_directory))
     files(target_directory)
 
+
 def tratar_path(target_directory):
     tg = target_directory.replace('/', '\\')
     return tg
 
+
 def files(target_directory):
-    #old_extension = '.bmp'
-    #new_extension = '.jpg'
+    # old_extension = '.bmp'
+    # new_extension = '.jpg'
     image_extension = ['.bmp', '.jpg', '.png', '.jpeg', '.gif']
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
@@ -30,16 +33,17 @@ def files(target_directory):
                 dir_new = dir.split('\\')
                 filename = dir_new[-1]
                 file, extension = splitext(filename)
-                dir = target_directory+root[1:]
+                dir = target_directory + root[1:]
                 if extension in image_extension:
                     convert(file, extension, dir, new_extension, filename)
+
 
 def convert(file, extension, dir, new_extension, filename):
     if extension != new_extension:
         try:
-            print(dir+'\\'+file+extension)
-            im = Image.open(dir+'\\'+file+extension)
-            im.save(dir+'\\'+file+new_extension)
+            print(os.path.join(dir, file + extension))
+            im = Image.open(os.path.join(dir, file + extension))
+            im.save(os.path.join(dir, file + new_extension))
             print('converted image: {}'.format(filename))
         except OSError as error:
             print("It's not possible to convert the file {}.\nError code: {}".format(
@@ -54,19 +58,21 @@ def do_backup(dir, file, extension):
     try:
         if not os.path.isdir(dir + './' + 'BKP_OLD_IMAGES'):
             # creates the backup directory
-            os.mkdir(dir+'./{}'.format('BKP_OLD_IMAGES'))
+            os.mkdir(dir + './{}'.format('BKP_OLD_IMAGES'))
             print('Created the {} directory'.format('BKP_OLD_IMAGES'))
     except:
         print("It wasn't possible to create the dir")
     try:
-        destino = dir + '\\BKP_OLD_IMAGES\\'+file+extension
-        source = dir+'\\'+file+extension
-        shutil.move(source, destino)
+        dest = os.path.join(dir, 'BKP_OLD_IMAGES', file + extension)
+        source = os.path.join(dir, file + extension)
+        shutil.move(source, dest)
     except:
         print("It wasn't possible to move")
 
+
 def do_exclusion(dir, file, extension):
-    source = dir+'\\'+file+extension
+    #source = dir + '\\' + file + extension
+    source = os.path.join(dir, file + extension)
     try:
         os.remove(source)
     except:
--- interface.py
+++ interface.py
@@ -6,16 +6,17 @@ root = Tk()
 root.title('Conversor de imagem')
 root.geometry('380x100')
 
+
 def browse_button():
     global folder_path
     global filename
     filename = filedialog.askdirectory()
     e.insert(0, filename)
 
-def ok_button(formato):
-    backup=var_backup.get()
-    convert_image.main(filename, backup, formato)
 
+def ok_button(formato):
+    backup = var_backup.get()
+    convert_image.main(filename, backup, formato)
 
 
 frame = LabelFrame(root)
@@ -25,25 +26,27 @@ MODES = [("BMP", ".bmp"), ("JPG", ".jpg"), ("PNG", ".png")]
 formato = StringVar()
 formato.set(".jpg")
 
-c=0
+c = 0
 for text, mode in MODES:
-    Radiobutton(root, text=text, variable=formato, value=mode).grid(row=0, column=c)
+    Radiobutton(root, text=text, variable=formato,
+                value=mode).grid(row=0, column=c)
     c += 1
 
-e = Entry(root, borderwidth=2,width=40, bg="white")
+e = Entry(root, borderwidth=2, width=40, bg="white")
 e.grid(row=2, column=1)
 
 
 folder_path = StringVar()
-lbl1 = Label(master=root,textvariable=folder_path)
+lbl1 = Label(master=root, textvariable=folder_path)
 lbl1.grid(row=2, column=0)
-button1 = Button(root,text="Browse", command=browse_button, anchor=W)
+button1 = Button(root, text="Browse", command=browse_button, anchor=W)
 button1.grid(row=2, column=2, padx=10)
-button2 = Button(root,text="OK", command= lambda: ok_button(formato.get()))
+button2 = Button(root, text="OK", command=lambda: ok_button(formato.get()))
 button2.grid(row=3, column=2, pady=10)
 
 var_backup = BooleanVar()
-c1 = Checkbutton(root, text='Fazer backup', variable = var_backup, onvalue=True, offvalue=False, anchor=E)
+c1 = Checkbutton(root, text='Fazer backup', variable=var_backup,
+                 onvalue=True, offvalue=False, anchor=E)
 c1.deselect()
 c1.grid(row=3, column=0, columnspan=5)
 
